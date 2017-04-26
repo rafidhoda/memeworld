182809867d629adfc9edc2dffd205d6e0a23f6fe
Rafid Hoda <rafidhoda@gmail.com>
2017-04-26 16:52:44 +0200

    Add links

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,5 +1,6 @@
 class MemesController < ApplicationController
   before_action :set_meme, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /memes
   # GET /memes.json
